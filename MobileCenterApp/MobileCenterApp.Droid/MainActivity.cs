8ed2245a05553befeddc20e17c7123ee9625f957
James Clancey <james.clancey@gmail.com>
2017-02-21 15:51:20 -0900

    Added analytics/crashes to Android

--- MobileCenterApp/MobileCenterApp.Droid/MainActivity.cs
+++ MobileCenterApp/MobileCenterApp.Droid/MainActivity.cs
@@ -7,17 +7,25 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace MobileCenterApp.Droid
 {
 	[Activity (Label = "MobileCenterApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
+		protected override void OnStart()
+		{
+			base.OnStart();
+			MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+		}
 		MobileCenterApp.App App;
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
+			MobileCenter.Configure("a635d99d-b70e-42ca-aa1e-61484e2fa40d");
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (App = new MobileCenterApp.App ());
 			App.OnInstallApp = (installUrl) =>
